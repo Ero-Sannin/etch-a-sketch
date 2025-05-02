67b6824ec8751132777e737a664b46d4615653ec
ero-sannin2 <rider.dragon320@gmail.com>
2025-05-02 14:30:59 +0545

    add opacity change

--- script.js
+++ script.js
@@ -1,35 +1,37 @@
 let createButton = document.querySelector("#createGrid");
-createButton.addEventListener("click", ()=>createGrid());
-let clearButton=document.querySelector("#clearGrid");
-clearButton.addEventListener("click",clearGrid)
+createButton.addEventListener("click", () => createGrid());
+let clearButton = document.querySelector("#clearGrid");
+clearButton.addEventListener("click", clearGrid);
+
 
 
 
 
 function createGrid(message = "enter number of squares(less than 100)") {
-    
+
     let numberOfSquares = Number(prompt(message));
 
-    while(isNaN(numberOfSquares)||numberOfSquares>100){
-        numberOfSquares=Number(prompt(message));
+    while (isNaN(numberOfSquares) || numberOfSquares > 100) {
+        numberOfSquares = Number(prompt(message));
     }
 
     let container = document.querySelector(".container");
-    container.innerHTML="";
+    container.innerHTML = "";
 
     for (let i = 1; i <= numberOfSquares; i++) {
         let row = document.createElement("div");
         row.classList.add(`row${i}`);
-        row.setAttribute("style","width:100%")
-        let height=100/numberOfSquares;
-        row.setAttribute("style",`height:${height}%`);
+        row.setAttribute("style", "width:100%")
+        let height = 100 / numberOfSquares;
+        row.setAttribute("style", `height:${height}%`);
         container.appendChild(row);
         for (let j = 1; j <= numberOfSquares; j++) {
             let column = document.createElement("div");
             column.classList.add(`row${i}`);
             column.classList.add(`column${j}`);
             column.classList.add("box");
-            column.addEventListener("mouseenter",changeStyle);
+            column.setAttribute("id", "10");
+            column.addEventListener("mouseenter", changeStyle);
             row.appendChild(column);
         }
     }
@@ -37,20 +39,35 @@ function createGrid(message = "enter number of squares(less than 100)") {
 
 }
 
-function changeStyle(event){
-    event.target.style.backgroundColor=`rgb(${random()},${random()},${random()})`;
+
+
+function changeStyle(event) {
+
+    opacity = Number(event.target.id) / 100;
+    event.target.style.background = generateRandomColor() + `,${opacity})`;
+
+    if (opacity < 1) {
+        opacity = (opacity * 100) + 10;
+        event.target.id = String(opacity);
+    }
 }
 
 
 
-function clearGrid(){
-    boxes=document.querySelectorAll(".box");
+function clearGrid() {
+    boxes = document.querySelectorAll(".box");
     boxes.forEach((box) => {
-        box.style.backgroundColor="bisque";
+        box.style.backgroundColor = "white";
+        box.style.border = "1px solid black";
+        box.style.opacity = "1";
     });
-    
+
 }
 
-function random(){
-    return (Math.floor(Math.random()*256));
+function random() {
+    return (Math.floor(Math.random() * 256));
+}
+
+function generateRandomColor() {
+    return `rgba(${random()},${random()},${random()}`;
 }
